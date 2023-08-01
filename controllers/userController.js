812c0597d92511ac16e7f80f9e3953eb66b3aa4d
savan2509 <psavan721@gmail.com>
2023-08-01 18:06:44 +0530

    logging-user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!'
+const User = require('./../models/UserModel');
+const catchAsync = require('./../units/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  // send message
+    res.status(200).json({
+      status: 'success',
+      result: users.length,
+      data: {
+        users
+      }
     });
-  };
+  });
+  
   exports.getUser = (req, res) => {
     res.status(500).json({
       status: 'error',
---
+++ models/UserModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name!']
+    },
+    email: {
+        type: String,
+        require: [true,'please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        require: [true, 'Please provide a password'],
+        minlength: 8,
+        select: false
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            //This only works on create and save !!!
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'password are not the same!'
+        }
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    // only run this function if password was actually modified
+    if(!this.isModified('password')) return next();
+
+    // hash the password with cost of 12
+this.password =  await bcrypt.hash(this.password, 12);
+
+// delete passwordConfirm field
+this.passwordConfirm = undefined;
+next();
+});
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword, 
+    userPassword
+    ) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
